d405b6f29a60e6586e986fd474662321a6d72cc8
ihavehands <zaneshook@gmail.com>
2020-06-08 15:51:06 -0700

    about me set up

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../../pages/Home";
+import About from "../../pages/About"
 
 export default () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
   </Switch>
 );
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "react"
+
+const About = ()=>{
+    return(<div>
+        <h1>About me hit</h1>
+    </div>)
+}
+
+export default About
